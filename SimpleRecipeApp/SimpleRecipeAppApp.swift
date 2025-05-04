151b5b6af47473249168adf5e1a285ec616e7e80
erncanyildirim <20227170062@ogr.oku.edu.tr>
2025-05-04 13:39:27 +0300

    Initial Commit

---
+++ SimpleRecipeApp/SimpleRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeAppApp.swift
+//  SimpleRecipeApp
+//
+//  Created by Eren Can YILDIRIM on 4.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimpleRecipeAppTests/SimpleRecipeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeAppTests.swift
+//  SimpleRecipeAppTests
+//
+//  Created by Eren Can YILDIRIM on 4.05.2025.
+//
+
+import Testing
+@testable import SimpleRecipeApp
+
+struct SimpleRecipeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
